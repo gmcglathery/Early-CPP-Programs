4934e07b262d997705d66cb8c6d0ce5919cc73e6
Greg McGlathery <76054937+gmcglathery@users.noreply.github.com>
2020-12-16 12:12:20 -0600

    Update return_even.cpp

--- return_even.cpp
+++ return_even.cpp
@@ -1,3 +1,9 @@
+/******************************************
+* Greg McGlathery
+* This is just a simple program to return
+* even numbers using recursion.
+******************************************/
+
 #include <iostream>
 
 using namespace std;
@@ -5,24 +11,24 @@ using namespace std;
 int count(int);
 int main()
 {
-    cout<<count(123456789);
+    cout << count(123456789);
     return 0;
 }
 
 int count(int nbr)
 {
-    if(nbr==0)
+    if(nbr == 0)
     {
         return 0;
     }
-    int last=nbr%10;
-    int first=nbr/10;
-    if (last%2==0)
+    int last = nbr % 10;
+    int first = nbr / 10;
+    if (last % 2 == 0)
     {
-        return 1+count(first);
+        return 1 + count(first);
     }
     else
     {
-        return 0+count(first);
+        return 0 + count(first);
     }
 }
